0b132bd211b81a8a8674e0a3c059ec045b91016f
Wojciechm-AdapptLimited <wojciech.channel@gmail.com>
2023-01-18 01:35:42 +0100

    smart_home files

--- raspberry/smart_home.py
+++ raspberry/smart_home.py
@@ -1,19 +1,30 @@
 from flask import Flask, request
 import json
+import mariadb
 
 app = Flask(__name__)
+
+@app.route('/post_data', methods=['POST'])
+def process_data():
+    content_type = request.headers.get('Content-Type')
+    if (content_type != 'application/json'):
+        return
+    
+        
+@app.route('/post_device', methods=['POST'])
+def process_data():
+    content_type = request.headers.get('Content-Type')
+    if (content_type != 'application/json'):
+        return
+    
+    data = request.json
+    
+    
 
 @app.route('/get_data', methods=['GET'])
 def get_data():
     # Fetch data from the database
-    data = {
-        "temperature": 25.0,
-        "humidity": 60.0,
-        "soilMoisture1": 40.0,
-        "soilMoisture2": 50.0,
-        "soilMoisture3": 30.0
-    }
-    return json.dumps(data)
+    return data
 
 if __name__ == 'main':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='192.168.4.1', port=8000, ssl_context=("server.crt", "server.key"))
---
+++ raspberry/smart_home.wsgi
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+import sys
+sys.path.insert(0, '/var/www/smart_home')
+from smart_home import app as application
