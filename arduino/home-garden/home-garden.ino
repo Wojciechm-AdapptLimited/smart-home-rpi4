7d4bce4b390e74fe115040bbad318266cc2cf9b2
Wojciech Maczka <wojciech.channel@gmail.com>
2023-01-18 23:00:26 +0100

    finished arduino uno code

--- arduino/home-garden/home-garden.ino
+++ arduino/home-garden/home-garden.ino
@@ -2,105 +2,103 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <CircularBuffer.h>
 
-#define SOILPIN1 A0
-#define SOILPIN2 A1
-#define SOILPIN3 A2
-#define DHTPIN 2
-#define DHTTYPE DHT22
-#define DELAY 1000
-#define ARRSIZE 20
+#define SOIL_PIN_1 A0
+#define SOIL_PIN_2 A1
+#define SOIL_PIN_3 A2
+#define DHT_PIN 2
+#define DHT_TYPE DHT22
+#define ARR_SIZE 20
 
-DHT_Unified dht(DHTPIN, DHTTYPE);
+DHT_Unified dht(DHT_PIN, DHT_TYPE);
 
-float temperature[ARRSIZE];
-float humidity[ARRSIZE];
-float soilMoisure1[ARRSIZE];
-float soilMoisure2[ARRSIZE];
-float soilMoisure3[ARRSIZE];
+CircularBuffer<float, ARR_SIZE> temperature;
+CircularBuffer<float, ARR_SIZE> humidity;
+CircularBuffer<float, ARR_SIZE> soilMoisture1;
+CircularBuffer<float, ARR_SIZE> soilMoisture2;
+CircularBuffer<float, ARR_SIZE> soilMoisture3;
 
-int idx = 0;
+const int AirValue = 840;
+const int WaterValue = 180;
+const int intervals = (AirValue - WaterValue) / 3;
+
+const char* moistureLevels[] = {"Too wet", "Very wet", "Wet", "Dry", "Too dry"};
+
+unsigned long previousMillis = 0;
+const unsigned long sampleInterval = 180000;
+
+StaticJsonDocument<128> doc;
+JsonObject sensors = doc.createNestedObject("sensors");
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   dht.begin();
+
+  doc["device"] = "c8:c9:a3:54:b1:ad";
 }
 
 void loop() {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis < sampleInterval) {
+    return;
+  }
+
+  previousMillis = currentMillis;
+
   sensors_event_t event;
+
   dht.temperature().getEvent(&event);
-  dht.humidity().getEvent(&event);
-
   if (!isnan(event.temperature)) {
-    temperature[idx] = event.temperature;
+    temperature.push(event.temperature);
   }
 
+  dht.humidity().getEvent(&event);
   if (!isnan(event.relative_humidity)) {
-    humidity[idx] = event.relative_humidity;
+    humidity.push(event.relative_humidity);
   }
 
-  soilMoisure1[idx] = analogRead(SOILPIN1);
-  soilMoisure2[idx] = analogRead(SOILPIN2);
-  soilMoisure3[idx] = analogRead(SOILPIN3);
+  soilMoisture1.push(analogRead(SOIL_PIN_1));
+  soilMoisture2.push(analogRead(SOIL_PIN_2));
+  soilMoisture3.push(analogRead(SOIL_PIN_3));
 
-  idx++;
+  if (soilMoisture1.isFull()) {
+    float avgTemperature = average(temperature);
+    float avgHumidity = average(humidity);
+    float avgSoilMoisture1 = average(soilMoisture1);
+    float avgSoilMoisture2 = average(soilMoisture2);
+    float avgSoilMoisture3 = average(soilMoisture3);
 
-  if (idx == ARRSIZE) {
-    Serial.println(avg(temperature, ARRSIZE));
-    Serial.println(avg(humidity, ARRSIZE));
-    Serial.println(avg(soilMoisure1, ARRSIZE));
-    Serial.println(avg(soilMoisure2, ARRSIZE));
-    Serial.println(avg(soilMoisure3, ARRSIZE));
+    sensors["DHT_22_TEMP"] = avgTemperature;
+    sensors["DHT_22_HUM"] = avgHumidity;
+    sensors["SOIL_1"] = getMoistureLevel(avgSoilMoisture1);
+    sensors["SOIL_2"] = getMoistureLevel(avgSoilMoisture2);
+    sensors["SOIL_3"] = getMoistureLevel(avgSoilMoisture3);
 
-    idx = 0;      
+    temperature.clear();
+    humidity.clear();
+    soilMoisture1.clear();
+    soilMoisture2.clear();
+    soilMoisture3.clear();
+
+    ArduinoJson6200_1::serializeJson(doc, Serial);
   }
-
-  delay(DELAY);
 }
 
-
-int argMin(float* arr, int arrSize) {
-  int imin = 0;
-  float min = arr[0];
-
-  for (int i = 0; i < arrSize; i++) {
-    if (arr[i] < min) {
-      min = arr[i];
-      imin = i;
-    }
-  }
-
-  return imin;
+const char* getMoistureLevel(float value) {
+  int level = value / intervals;
+  level = constrain(level, 0, 4);
+  return moistureLevels[level];
 }
 
-int argMax(float* arr, int arrSize) {
-  int imax = 0;
-  float max = arr[0];
+float average(CircularBuffer<float, ARR_SIZE>& buffer) {
+  float avg = 0.0;
 
-  for (int i = 0; i < arrSize; i++) {
-    if (arr[i] < max) {
-      max = arr[i];
-      imax = i;
-    }
+  using index_t = CircularBuffer<float, ARR_SIZE>::index_t;
+  for (index_t i = 0; i < buffer.size(); i++) {
+    avg += buffer[i] / (float)buffer.size();
   }
 
-  return imax;
-}
-
-float avg(float* arr, int arrSize) {
-  int imin = argMin(arr, arrSize);
-  int imax = argMax(arr, arrSize);
-  float total = 0.0;
-
-  for (int i = 0; i < arrSize; i++) {
-    if (i == imin || i == imax) {
-      total += arr[i];
-    }
-  }
-
-  if (imin == imax) {
-    return total/(arrSize - 1);    
-  }
-
-  return total/(arrSize - 1);  
+  return avg;
 }
